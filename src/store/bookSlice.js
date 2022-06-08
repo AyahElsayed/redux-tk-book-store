fcb7db7ae2691a72d3978788c31c89c923876691
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-06-08 18:14:05 +0200

    createAsyncThunk

--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -1,9 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+const getBooks = createAsyncThunk('book/getBooks', async (_, thunkAPI) => {
+  try {
+    const res = await fetch('http://localhost:3005/books')
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 const bookSlice = createSlice({
   name: 'Book',
   initialState: { books: null },
-  reducers:{},
+  reducers: {},
 })
 
 export default bookSlice.reducer
