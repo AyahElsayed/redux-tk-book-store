ff89380815398af11f70e4bdb504bf4df6f7bc10
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-06-09 16:54:33 +0200

    incase there is no books

--- src/components/Book/BooksList.js
+++ src/components/Book/BooksList.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
 const BooksList = ({ isLoading, books }) => {
-  const bookList = books.map((item) => (
-    <li className='list-group-item d-flex  justify-content-between align-items-center' key={item.id}>
-      <div>{item.title}</div>
-      <div className='btn-group' role='group'>
-        <button type='button' className='btn btn-primary'>
-          Read
-        </button>
-        <button type='button' className='btn btn-danger'>
-          Delete
-        </button>
-      </div>
-    </li>
-  ))
+
+  const bookList =
+    books.length > 0 ?
+      books.map((item) => (
+        <li className='list-group-item d-flex  justify-content-between align-items-center' key={item.id}>
+          <div>{item.title}</div>
+          <div className='btn-group' role='group'>
+            <button type='button' className='btn btn-primary'>
+              Read
+            </button>
+            <button type='button' className='btn btn-danger'>
+              Delete
+            </button>
+          </div>
+        </li>
+      ))
+      :
+      'there is no books selected'
   return (
     <div>
       <h2>Books List</h2>
