edd4b53a19bf65093a28d46032a9e61ce6c43f88
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-06-09 16:49:50 +0200

    thunkAPI RejectWithValue

--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -1,18 +1,19 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 export const getBooks = createAsyncThunk('book/getBooks', async (_, thunkAPI) => { // '_' mean that there is an arg but not used
+  const { rejectWithValue } = thunkAPI
   try {
     const res = await fetch('http://localhost:3005/books')
     const data = await res.json()
     return data
   } catch (error) {
-    console.error(error)
+    return rejectWithValue(error.message)
   }
 })
 
 const bookSlice = createSlice({
   name: 'Book',
-  initialState: { books: [], isLoading: false },
+  initialState: { books: [], isLoading: false, error: '' },
   extraReducers: {
     [getBooks.pending]: (state, action) => {
       state.isLoading = true
@@ -25,6 +26,7 @@ const bookSlice = createSlice({
     },
     [getBooks.rejected]: (state, action) => {
       state.isLoading = false
+      state.error = action.payload
       console.log(action)
     }
   },
